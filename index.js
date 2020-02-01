84bce907dd7dd20bbba6f851c236ab9bb9e1b151
Brian Salemi <briandsalemi@gmail.com>
2020-02-01 17:49:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 /* Given Code, don't edit */
+function handleClick(e) {
+  const timeString = document.getElementById('time').value
+  displayMessage(greet(timeString))
+}
+function greet(time){
+  if(time < "12:00"){
+    return "Good Morning"
+  }
+}
